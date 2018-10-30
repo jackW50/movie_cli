5e0124279c2891e15e4fd20d9df6c16e2539b77b
John Werenski <jtwerenski@gmail.com>
2018-10-30 17:30:02 +0000

    Modified Scraper

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'pry'
 gem 'require_all'
+gem 'nokogiri'
+gem 'poltergeist'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,23 @@
 require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require 'capybara/poltergeist'
+
 
 class Scraper
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path 
+  end 
+  
+  def theater_scraper
+    html = open(path)
+    doc = Nokogiri::HTML(html)
+    #binding.pry
+  end 
+  
+  def theater_parser
+  end 
   
 end 
