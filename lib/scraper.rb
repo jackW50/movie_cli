0d16bb488aa4d647294421dcb48466ad0dc4aee8
John Werenski <jtwerenski@gmail.com>
2018-10-30 19:16:00 +0000

    adding Nokogiri

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'capybara/poltergeist'
 
-
+# http://www.movies.com/movie-times/63015-movie-times
 class Scraper
   attr_accessor :path 
   
@@ -14,9 +14,21 @@ class Scraper
   def theater_scraper
     html = open(path)
     doc = Nokogiri::HTML(html)
+    doc.css("ul li")
     #binding.pry
   end 
   
+  def theater_name
+    doc.css("ul li div.theaterInfo h2 a").text 
+  end 
+  
+  def theater_location
+    doc.css("ul li div.theaterInfo p")[2].text.split("\r")
+  end 
+  
+  def theater_movies
+  end 
+    
   def theater_parser
   end 
   
