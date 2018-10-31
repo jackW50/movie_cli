d6bc57fb1cd06cffd15a5ff509978585520ecdc0
John Werenski <jtwerenski@gmail.com>
2018-10-31 16:49:59 +0000

    Additional Parsing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,17 +15,32 @@ class Scraper
     binding pry
     html = open(path)
     doc = Nokogiri::HTML(html)
-    doc.css("ul li")
+    doc.css("ul.theaterList li")
     #binding.pry
   end 
   
+  def theater_name_parser 
+    theater_scraper.collect {|theater| theater.css("div.theaterInfo h2 a").text}.uniq 
+    #return array of theater names 
+  end
+  
   def theater_name
-    doc.css("ul li div.theaterInfo h2 a").text 
+    doc.css("div.theaterInfo h2 a").text 
+  end 
+  
+  def theater_location_parser
+    theater_scraper.collect {|theater| theater.css("div.theaterInfo p").text.strip}.uniq
+    # an array for each theaters location.
   end 
   
   def theater_location
-    doc.css("ul li div.theaterInfo p")[2].text.split("\r")
+     a =doc.css("div.theaterInfo p")[2].text.strip.split("\r").uniq
+     a.collect {|i| i.match(/\w+/)}
+     a 
   end 
+  
+  def movie_listings_parser 
+    a = theater_scraper.css(ul.movieListings li)
   
   def theater_movies
     movie_name = doc.css("ul.movieListings li div.info p")[0].text.strip 
